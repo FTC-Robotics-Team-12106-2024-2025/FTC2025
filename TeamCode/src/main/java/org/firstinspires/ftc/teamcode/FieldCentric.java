6b20d2dc635d000d3b0c31af165c1e4386f74729
ItsFireninja <noahblin@gmail.com>
2025-01-21 17:10:42 -0600

    Changed power, also Adi push into main because I'm way too tired of using the road-runner branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
@@ -117,7 +117,7 @@ public class FieldCentric extends LinearOpMode {
 
         jointMotor.setTargetPosition(targetLiftPosition);
         jointMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        jointMotor.setPower(.8);
+        jointMotor.setPower(1);
 
             if (manipulatorGamepad.dpad_up) {
                 targetLiftPosition = jointLiftUpPosition;
