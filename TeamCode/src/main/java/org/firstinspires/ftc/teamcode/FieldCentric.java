3d9b790350839092702b6a649a43473e89679fc5
ItsFireninja <noahblin@gmail.com>
2025-01-23 17:07:36 -0600

    Stupid branch name. Re-added joint code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
@@ -11,9 +11,6 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
 
 @TeleOp
 
@@ -23,7 +20,7 @@ public class FieldCentric extends LinearOpMode {
 
      // Variables
 
-     public int jointPose = 0;
+     public int armPose = 0;
      public int slidePose = 0;
      public float wristPose = 0;
      public int Test;
@@ -31,9 +28,9 @@ public class FieldCentric extends LinearOpMode {
 
      int colorFilter = 0; // 0 = filter for red, 1 = for blue, 2 = for yellow
 
-     public int jointLiftUpPosition = -8000;//change this value according to encoder
-     public int jointLiftDownPosition = 500;//change this value according to encoder
-     public int targetLiftPosition = 0;//updates in if statement, DO NOT CHANGE
+     public int armUpPose = -450;//change this value according to encoder
+     public int armDownPose = -1550;//change this value according to encoder
+     public int targetArmPose = 0;//updates in if statement, DO NOT CHANGE
 
 
 
@@ -42,16 +39,16 @@ public class FieldCentric extends LinearOpMode {
     //Defines the motor
     public void runOpMode() {
         // Drive Motors
-        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft"); //Port 0
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft"); // Port 0
         DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight"); //Port 1
         DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-        DcMotor jointMotor = hardwareMap.get(DcMotor.class,"jointMotor");
-        DcMotor slide = hardwareMap.get(DcMotor.class, "slide");
+        DcMotor armMotor = hardwareMap.get(DcMotor.class,"armMotor");
+        DcMotor slideMotor = hardwareMap.get(DcMotor.class, "slide");
         CRServo intakeOne = hardwareMap.get(CRServo.class,"intakeOne");
         CRServo intakeTwo = hardwareMap.get(CRServo.class,"intakeTwo");
-        Servo wrist = hardwareMap.get(Servo.class,"wrist");
+        Servo wristServo = hardwareMap.get(Servo.class,"wrist");
 
         ColorSensor color = hardwareMap.get(ColorSensor.class, "color");
         DistanceSensor colorDistance = hardwareMap.get(DistanceSensor.class, "color");
@@ -59,26 +56,26 @@ public class FieldCentric extends LinearOpMode {
         boolean sortActive = false;
 
         //BRAKES
-        jointMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         //encoders
         //slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
        // jointMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 //slide
-        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         //joint
-        jointMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 //        // Setting Positions
 //        slide.setTargetPosition(targetLiftPosition);
 //        slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 //        slide.setPower(.8);
 //
-//        jointMotor.setTargetPosition(targetLiftPosition);
-//        jointMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        jointMotor.setPower(.8);
+        armMotor.setTargetPosition(targetArmPose);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setPower(.8);
 
         // Gyroscope
         BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
@@ -103,7 +100,7 @@ public class FieldCentric extends LinearOpMode {
             driveGamepad.copy(gamepad1);
             manipulatorGamepad.copy(gamepad2);
 
-            Test = jointMotor.getCurrentPosition();
+            Test = armMotor.getCurrentPosition();
 
 
 //            jointMotor.setTargetPosition(0);
@@ -115,25 +112,26 @@ public class FieldCentric extends LinearOpMode {
 //        slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 //        slide.setPower(.8);
 
-        jointMotor.setTargetPosition(targetLiftPosition);
-        jointMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        jointMotor.setPower(1);
+        armMotor.setTargetPosition(targetArmPose);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setPower(1);
 
             if (manipulatorGamepad.dpad_up) {
-                targetLiftPosition = jointLiftUpPosition;
+                targetArmPose = armUpPose;
 
             } else if (manipulatorGamepad.dpad_down) {
-                targetLiftPosition = jointLiftDownPosition;
+                targetArmPose = armDownPose;
 
             }
 
             //
             // Drive
             //
-            double wheelCPR = 423.2116; //Counts per revolution
+            double wheelCPR = 423.2116; // Counts per revolution
             double linearCPR = 72.1;
             float y = -driveGamepad.left_stick_y;
             float x = driveGamepad.left_stick_x;
+            float slowX = driveGamepad.right_stick_x/4;
 
             if (driveGamepad.dpad_left) {
                  x = -0.25f;
@@ -152,7 +150,7 @@ public class FieldCentric extends LinearOpMode {
 //          boolean honk = driveGamepad.left_stick_button;
             double rotateRight = driveGamepad.right_trigger;
             double rotateLeft = driveGamepad.left_trigger;
-            double rx = rotateRight-rotateLeft;
+            double rx = rotateRight-rotateLeft+slowX;
             double xRot = x * Math.cos(currentHeading) - y * Math.sin(currentHeading);
             double yRot = y * Math.cos(currentHeading) + x * Math.sin(currentHeading);
 
@@ -166,26 +164,26 @@ public class FieldCentric extends LinearOpMode {
             double maxNumber = Math.max(Math.abs(xRot)+Math.abs(yRot)+Math.abs(rx),1);
 
             //powers the motor for wheels
-            frontLeft.setPower(fl / maxNumber * .85);
-            frontRight.setPower(fr / maxNumber * .85);
-            backLeft.setPower(bl / maxNumber * .85);
-            backRight.setPower(br / maxNumber * .85);
+            frontLeft.setPower(fl / maxNumber * .7);
+            frontRight.setPower(fr / maxNumber * 7);
+            backLeft.setPower(bl / maxNumber * .7);
+            backRight.setPower(br / maxNumber * .7);
 
             //
-            // Arm Angle (Joint)
+            // Arm Angle
             //
 
-            jointPose += (-manipulatorGamepad.right_stick_y * 100);
+            armPose += (-manipulatorGamepad.right_stick_y * 100);
 
-            if (jointPose > 0) {
-                jointPose = 0;
+            if (armPose > 0) {
+                armPose = 0;
             }
-            if (jointPose < -1570) {
-                jointPose = -1570;
+            if (armPose < -1570) {
+                armPose = -1570;
             }
 
-            jointMotor.setTargetPosition(jointPose);
-            telemetry.addData("Measured Arm Position: ", jointPose);
+            armMotor.setTargetPosition(armPose);
+            telemetry.addData("Arm Angular Position: ", armPose);
 
 
             //
@@ -201,88 +199,81 @@ public class FieldCentric extends LinearOpMode {
                 slidePose = 6100;
             }
 
-            //Joint
-           telemetry.addData("jointP", jointPose);
-
-            slide.setTargetPosition(slidePose);
-            telemetry.addData("SlidePose: ", slidePose);
+            slideMotor.setTargetPosition(slidePose);
+            telemetry.addData("Slide Linear Position: ", slidePose);
 
             //
             // Wrist
             //
             if (manipulatorGamepad.left_bumper) {
-                wristPose += 0.1f;
-            }
-            if (manipulatorGamepad.right_bumper) {
-                wristPose -= 0.1f;
-            }
-
-            if (wristPose < 0) {
-                wristPose = 0;
-            }
-            if (wristPose > 1) {
                 wristPose = 1;
             }
-            wrist.setPosition(wristPose);
-            telemetry.addData("WristPose",wristPose);
+            if (manipulatorGamepad.right_bumper) {
+                wristPose = 0;
+            }
+            if (manipulatorGamepad.guide) {
+                wristPose = 0.5f;
+            }
+
+            wristServo.setPosition(wristPose);
+            telemetry.addData("WristPose", wristPose);
 
             //
             // Intake
             //
             if (manipulatorGamepad.left_trigger >= .9) {
-                intakeOne.setPower(0.75);
-                intakeTwo.setPower(-0.75);
+                intakeOne.setPower(-0.75);
+                intakeTwo.setPower(0.75);
             }
            if (manipulatorGamepad.right_trigger >= .9) {
-               intakeOne.setPower(-0.75);
-               intakeTwo.setPower(0.75);
+               intakeOne.setPower(0.75);
+               intakeTwo.setPower(-0.75);
            }
-           //Emergency Stop
-           if (manipulatorGamepad.options) {
-               intakeOne.setPower(0);
-               intakeTwo.setPower(0);
-           }
+
+           intakeOne.setPower(0);
+           intakeTwo.setPower(0);
+
            if (manipulatorGamepad.circle){ // for async
                sortActive = false;
            }
 
-           if (sortActive) {
-               manipulatorGamepad.setLedColor(sampleLook[colorFilter][0], sampleLook[colorFilter][1], sampleLook[colorFilter][2], 1000);
-               intakeOne.setPower(-0.75);
-               intakeTwo.setPower(-0.75); // powers might be wrong
-               int red = thresholds[colorFilter][0];
-               int blue = thresholds[colorFilter][1];
-               int green = thresholds[colorFilter][2];
-               if (colorDistance.getDistance(DistanceUnit.CM) < 2.0) {
-                   intakeOne.setPower(0);
-                   intakeTwo.setPower(0);           // tolerance of 10 (can be changed)
-                   if (Math.abs(color.red() - red) < 10 && Math.abs(color.blue() - blue) < 10 && Math.abs(color.green() - green) < 10) {
-                       driveGamepad.rumble(50);
-                       manipulatorGamepad.rumble(50);
-                       manipulatorGamepad.setLedColor(sampleHave[colorFilter][0], sampleHave[colorFilter][1], sampleHave[colorFilter][2], 1000);
-                       sortActive = false;
-                   } else {
-                       timer.reset();// might reset too much
-                       intakeOne.setPower(0.75);
-                       intakeTwo.setPower(0.75);
-                       if (timer.milliseconds() >= 500) { // might not resolve if it checks it immediately (checks right after timer is on and never flips direction)
-                           intakeOne.setPower(-0.75);
-                           intakeTwo.setPower(-0.75);
-                       }
-                   }
-               }
-           }
-           //ColorSort
-
-           if (manipulatorGamepad.share) {
-               colorFilter++;
-               if (colorFilter > 2) {
-                   colorFilter = 0;
-               }
-               telemetry.clearAll();// Updates which type of color sort it is; other info will be printed due to it being a while loop
-               telemetry.addData(colorData[colorFilter], colorFilter);
-               telemetry.update();
-           }
+//           if (sortActive) {
+//               manipulatorGamepad.setLedColor(sampleLook[colorFilter][0], sampleLook[colorFilter][1], sampleLook[colorFilter][2], 1000);
+//               intakeOne.setPower(-0.75);
+//               intakeTwo.setPower(-0.75); // powers might be wrong
+//               int red = thresholds[colorFilter][0];
+//               int blue = thresholds[colorFilter][1];
+//               int green = thresholds[colorFilter][2];
+//               if (colorDistance.getDistance(DistanceUnit.CM) < 2.0) {
+//                   intakeOne.setPower(0);
+//                   intakeTwo.setPower(0);           // tolerance of 10 (can be changed)
+//                   if (Math.abs(color.red() - red) < 10 && Math.abs(color.blue() - blue) < 10 && Math.abs(color.green() - green) < 10) {
+//                       driveGamepad.rumble(50);
+//                       manipulatorGamepad.rumble(50);
+//                       manipulatorGamepad.setLedColor(sampleHave[colorFilter][0], sampleHave[colorFilter][1], sampleHave[colorFilter][2], 1000);
+//                       sortActive = false;
+//                   } else {
+//                       timer.reset();// might reset too much
+//                       intakeOne.setPower(0.75);
+//                       intakeTwo.setPower(0.75);
+//                       if (timer.milliseconds() >= 500) { // might not resolve if it checks it immediately (checks right after timer is on and never flips direction)
+//                           intakeOne.setPower(-0.75);
+//                           intakeTwo.setPower(-0.75);
+//                       }
+//                   }
+//               }
+//           }
+//           //ColorSort
+//
+//           if (manipulatorGamepad.share) {
+//               colorFilter++;
+//               if (colorFilter > 2) {
+//                   colorFilter = 0;
+//               }
+//               telemetry.clearAll();// Updates which type of color sort it is; other info will be printed due to it being a while loop
+//               telemetry.addData(colorData[colorFilter], colorFilter);
+//               telemetry.update();
+//           }
 
             telemetry.addData("Red",color.red());
             telemetry.addData("Blue",color.blue());
